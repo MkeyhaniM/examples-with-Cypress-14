53b42585b6c934a37ad5abdad9ce2b860836c343
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-04-18 15:46:44 +0330

    install shadcn ui and create a button

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+// export function getInitials(title) {
+//   return title
+//     .split(" ")
+//     .filter((word) => word.length > 0)
+//     .map((word) => word[0].toUpperCase())
+//     .join("");
+// }
