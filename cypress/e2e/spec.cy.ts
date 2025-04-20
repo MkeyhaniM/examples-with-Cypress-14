00d7c1b4b5bb88bddd049f373c7e6eb993a8bd60
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-04-20 21:44:31 +0330

    write a test check todo

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe("N2N", () => {
-  it("should visit", () => {
-    cy.visit("http://localhost:5174/");
+describe("Check todo app", () => {
+  it("get todo app ", () => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("check elements", () => {
+    cy.get("h1").should("contain", "Todo App");
+    cy.get("p").should(
+      "contain",
+      "No tasks available. Start by adding a new task!"
+    );
+  });
+
+  it("add a task to app", () => {
+    cy.get("input[type='text']").type("go to gym");
+  });
+
+  it("submit to add task", () => {
+    cy.get("button[type='submit']").click();
   });
 });
